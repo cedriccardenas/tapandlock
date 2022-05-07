2d31cc289f4d058f2730532d92c873ae96c40606
cedriccardenas <101538611+cedriccardenas@users.noreply.github.com>
2022-05-07 10:58:18 +0800

    IT Project

---
+++ js/Questions.js
@@ -0,0 +1,184 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = 
+[
+  {
+    numb: 1,
+    question: "Creating a password for any account must be:",
+    answer: "All of the above",
+    options: [
+      "All numbers",
+      "Some special characters",
+      "With capital letters",
+      "All of the above"
+    ]
+ },
+  {
+    numb: 2,
+    question: "You accidentally click the ad pop-up in the internet browser:",
+    answer: "Install an ad blocker",
+    options: [
+      "Install an ad blocker",
+      "Don't use the browser",
+      "Just ignore the ads",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "There is a lot of friend request from strangers on your social media account:",
+    answer: "Do not accept their request if you dont know the person",
+    options: [
+      "Only accept the request from good looking individual",
+      "Do not accept their request if you dont know the person",
+      "Create new social media account",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 4,
+    question: "The most risky info to put in a social media account profile?",
+    answer: "Address, Contact number, Financial information",
+    options: [
+      "Name, Age, Birthdate",
+      "Nickname, Relationship status, Profession",
+      "Address, Contact number, Financial information",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is first thing to consider when creating a post online?",
+    answer: "Think before you click",
+    options: [
+      "Click before you think",
+      "Do not post anything online",
+      "Think before you click",
+      "All of the above"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "What is the best thing to do to identify a fake news?",
+    answer: "Check the source, author, facts, comments and maintain a critical mindset",
+    options: [
+      "Check the source, author, facts, comments and maintain a critical mindset",
+      "Every single thing on the internet is true and reliable.",
+      "Just trust your own beliefs and instinct.",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What if someone asks for your IP address?",
+    answer: "An IP address is not actually sensitive information but be careful with whom you share it.",
+    options: [
+      "IP Address is not that important because it is not your home address, you can provide it to anyone.",
+      "Say no",
+      "An IP address is not actually sensitive information but be careful with whom you share it.",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "There is an online friend who keeps harrassing you online, What would you do?",
+    answer: "Stop all communication with the harasser, keep all evidence of harassment and report it to the police.",
+    options: [
+      "If it is good looking person then just give in, there is nothing to lose if it is online",
+      "Stop all communication with the harasser, keep all evidence of harassment and report it to the police.",
+      "Just kill the harasser",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 9,
+    question: "You see someone who impersonates you online:",
+    answer: "Let your friends and parents know about this fake account and click the report button",
+    options: [
+      "Actually, that is an achievement you are so good looking person to have a poser account",
+      "Let him/her feel your wrath then fight him/her in online conversation",
+      "Ignore them",
+      "Let your friends and parents know about this fake account and click the report button"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What to do when you receive a phising email?",
+    answer: "Do not open the email and never click any links or download attachment that appear in the message",
+    options: [
+      "Do not open the email and never click any links or download attachment that appear in the message",
+      "Open the email and click the available links out of curiousity",
+      "Forward it to another person",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "A scheme that involves a message from a lottery company out of the blue, they claim to tell you you've won and/or recieved a grand prize in a lottery/competition you didn't enter",
+    answer: "LOTTERY SCAM",
+    options: [
+      "LOTTERY SCAM",
+      "INHERITANCE SCAM",
+      "CARD SKIMMING",
+      "LOVE/ROMANCE SCAM"
+    ]
+  },
+  {
+    numb: 12,
+    question: "This scheme targets emotionally vulnerable people by making fake profiles on dating sites and social media",
+    answer: "LOVE/ROMANCE SCAM",
+    options: [ 
+      "BOILER ROOM",
+      "INHERITANCE SCAM",
+      "LOVE/ROMANCE SCAM",
+      "LOTTERY SCAM"
+    ]
+  },
+  {
+    numb: 13,
+    question: "The electronic posting of mean-spirited messages about a person often done anonymously",
+    answer: "Cyber Bullying",
+    options: [
+      "Cyber Bullying",
+      "Phishing",
+      "Cyber Stalking",
+      "BOILER ROOM"
+    ]
+  },
+  {
+    numb: 14,
+    question: "This scheme that involves a message from someone out of the blue claiming you're a benifactor or heir to a wealthy person who has supposedly died without a will",
+    answer: "INHERITANCE SCAM",
+    options: [
+      "CARD SKIMMING",
+      "LOVE/ROMANCE SCAM",
+      "INHERITANCE SCAM",
+      "BOILER ROOM"
+    ]
+  },
+  {
+    numb: 15,
+    question: "false information that is spread, regardless of whether there is intent to mislead",
+    answer: "Misinformation",
+    options: [
+      "Cyber Stalking",
+      "Phishing",
+      "CARD SKIMMING",
+      "Misinformation"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/TestingQA.js
@@ -0,0 +1,126 @@
+// creating questions and answers
+
+let questions = [
+    {
+    numb: 1,
+    question: "When making a password for your social media account, what is the first one to consider?",
+    answer: "All of the above",
+    options: [
+      "All numbers",
+      "Some special characters",
+      "With capital letters",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "You accidentally click the ad pop-up in the internet browser. What is the best option to avoid this scenario?",
+    answer: "Install an adblocker",
+    options: [
+      "Install an adblocker",
+      "Don't use the browser",
+      "Just ignore the ads",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "There is a lot of friend request from strangers on your social media account. What is the best thing to do in this scenario?",
+    answer: "Do not accept their request if you dont know the person",
+    options: [
+      "Only accept the request from good looking indiviualr",
+      "Do not accept their request if you dont know the person",
+      "Create new social media account",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What personal information that is the most risky to put in a social media account profile?",
+    answer: "Address, Contact number, Financial information",
+    options: [
+      "Name, Age, Birthdate",
+      "Nickname, Relationship status, Profession",
+      "Address, Contact number, Financial information",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is first thing to consider when creating a post online?",
+    answer: "Think before you click",
+    options: [
+      "Click before you think",
+      "Do not post anything online",
+      "Think before you click",
+      "All of the above"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What is the best thing to do to identify a fake news?",
+    answer: "Check the source, author, facts, comments and maintain a critical mindset",
+    options: [
+      "Check the source, author, facts, comments and maintain a critical mindset",
+      "Every single thing on the internet is true and reliable.",
+      "Just trust your own beliefs and instinct.",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What if someone asks for your IP address?",
+    answer: "An IP address is not actually sensitive information but be careful with whom you share it.",
+    options: [
+      "IP Address is not that important because it is not your home address, you can provide it to anyone.",
+      "Say no",
+      "An IP address is not actually sensitive information but be careful with whom you share it.",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "There is a sexual abuser who keeps harrassing you online. What will you do in this scenario?",
+    answer: "Stop all communication with the harasser, keep all evidence of harassment and report it to the police.",
+    options: [
+      "If it is good looking person then just give in, there is nothing to lose if it is online",
+      "Stop all communication with the harasser, keep all evidence of harassment and report it to the police.",
+      "Just kill the harasser",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 9,
+    question: "You see someone who impersonates you online. What is the best thing to do in this scenario?",
+    answer: "Let your friends and parents know about this fake account and click the report button",
+    options: [
+      "Let your friends and parents know about this fake account and click the report button",
+      "Actually, that is an achievement you are so good looking person to have a poser account",
+      "Let him/her feel your wrath then fight him/her in online conversation",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What to do when you receive a phising email?",
+    answer: "Do not open the email and never click any links or download attachment that appear in the message",
+    options: [
+      "Open the email and click the available links because you are stupid",
+      "Forward it to another person",
+      "Do not open the email and never click any links or download attachment that appear in the message",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "It's defined as a confidence game or other fraudulent scheme, especially for making a quick profit",
+    answer: "Online Scam",
+    options: [
+      "Plagiarism",
+      "Cyber Bullying",
+      "Pishing",
+      "Online Scam"
+    ]
+  },
+];
